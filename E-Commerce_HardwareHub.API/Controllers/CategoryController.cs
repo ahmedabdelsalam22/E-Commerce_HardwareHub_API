9154a39788adcb8d1b881990c5d323b486ef80a3
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-12-12 19:50:48 +0200

    create CategoryController and APIResponse , MappingConfig

---
+++ E-Commerce_HardwareHub.API/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HardwareHub.Data.Services.UOW;
+using HardwareHub.Models.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace E_Commerce_HardwareHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly APIResponse _apiResponse;
+
+        public CategoryController(IUnitOfWork unitOfWork, IMapper mapper, APIResponse apiResponse)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _apiResponse = new APIResponse();
+        }
+
+
+    }
+}
---
+++ HardwareHub.Models/Models/APIResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareHub.Models.Models
+{
+    public class APIResponse
+    {
+        public bool IsSuccess { get; set; }
+        public List<string> Message { get; set; }
+        public object? Result { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
