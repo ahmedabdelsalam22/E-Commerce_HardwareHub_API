0d6d3d0043e7fd2575cd1fa49be3b7ef7e6842e7
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-12-12 15:15:40 +0200

    create UnitOfWork

---
+++ HardwareHub.Data/Services/UOW/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using HardwareHub.Data.Services.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareHub.Data.Services.UOW
+{
+    public interface IUnitOfWork
+    {
+        public IProductRepository productRepository { get; }
+
+    }
+}
---
+++ HardwareHub.Data/Services/UOW/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using HardwareHub.Data.Services.IRepositories;
+using HardwareHub.Data.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareHub.Data.Services.UOW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public UnitOfWork(ApplicationDbContext context) 
+        {
+            _context = context;
+            productRepository = new ProductRepository(_context);
+        }
+
+        public IProductRepository productRepository { get; private set; }
+    }
+}
