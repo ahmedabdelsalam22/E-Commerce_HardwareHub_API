79f445ac0b8d40bea86f94aa9f034686a0e8c725
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-12-12 15:12:27 +0200

    create ProductRepository

---
+++ HardwareHub.Data/Services/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using HardwareHub.Data.Services.IRepositories;
+using HardwareHub.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareHub.Data.Services.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task Update(Product product)
+        {
+            _context.Update(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
