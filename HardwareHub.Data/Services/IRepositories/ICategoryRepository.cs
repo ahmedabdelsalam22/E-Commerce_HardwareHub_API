4a72335b1d6cfba550a957e5e4e55bb5f78753bc
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-12-12 15:18:11 +0200

    create CategoryRepository

---
+++ HardwareHub.Data/Services/IRepositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using HardwareHub.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareHub.Data.Services.IRepositories
+{
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+        Task Update(Category category);
+    }
+}
---
+++ HardwareHub.Data/Services/Repositories/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using HardwareHub.Data.Services.IRepositories;
+using HardwareHub.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareHub.Data.Services.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task Update(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
