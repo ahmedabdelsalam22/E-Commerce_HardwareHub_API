30fe339088e4e4e010155a52710b56fbb50f3ff9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-12-22 04:45:38 +0200

    create needed models for identity process

---
+++ HardwareHub.Models/Dtos/RegisterRequestDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareHub.Models.Dtos
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(8, ErrorMessage = "password must contains 8 characters and numbers at least")]
+        public string Password { get; set; }
+    }
+}
